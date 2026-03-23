c430a865e96c19a20eaee78942b791d4f156c320
Vladyslav Verbytskyi <s32804@pjwstk.edu.pl>
2026-03-24 00:19:15 +0100

    Service functionality progress

--- APBD-Cw2-s32804/IService.cs
+++ APBD-Cw2-s32804/IService.cs
@@ -2,5 +2,19 @@ namespace APBD_Cw2_s32804;
 
 public interface IService
 {
-    
+    void AddUser(User user);
+    void AddEquipment(Equipment equipment);
+
+    List<Equipment> GetAllEquipment();
+    List<Equipment> GetAvailableEquipment();
+
+    void Borrow(User user, Equipment equipment, int days);
+    void Return(Equipment equipment);
+
+    void MarkUnavailable(Equipment equipment);
+
+    List<Loan> GetUserLoans(User user);
+    List<Loan> GetLateLoans();
+
+    void GenerateReport();
 }
--- APBD-Cw2-s32804/Service.cs
+++ APBD-Cw2-s32804/Service.cs
@@ -2,5 +2,27 @@ namespace APBD_Cw2_s32804;
 
 public class Service : IService
 {
-    
+    private List<User> users = new List<User>();
+    private List<Equipment> equipmentList = new List<Equipment>();
+    private List<Loan> loans = new List<Loan>();
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+
+    public void AddEquipment(Equipment equipment)
+    {
+        equipmentList.Add(equipment);
+    }
+
+    public List<Equipment> GetAllEquipment()
+    {
+        return equipmentList;
+    }
+
+    public List<Equipment> GetAvailableEquipment()
+    {
+        return equipmentList.Where(e => e.IsAvailable).ToList();
+    }
 }
