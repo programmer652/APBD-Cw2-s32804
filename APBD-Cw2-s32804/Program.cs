b63c09bcacc35a8af24c6423bfb792812289883c
Vladyslav Verbytskyi <s32804@pjwstk.edu.pl>
2026-03-24 15:39:13 +0100

    main class work in progress

--- APBD-Cw2-s32804/Program.cs
+++ APBD-Cw2-s32804/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace APBD_Cw2_s32804;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting program");
+        
+        Camera camera = new Camera("");
+        
+
+    }
+}
